47569817554ebb85492aa1ff1ed99ec6bf40e1e2
Manovate <manovatetechnologies@gmail.com>
2025-12-31 06:10:57 -0800

    first commit

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,7 +133,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'manovatetechnologies@gmail.com'
-EMAIL_HOST_PASSWORD = 'ykrhtdgoicpjflsh'
-
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
